f6babc9bf53189562c8ef6be492e5428d17ea95e
mahtamtd <manhtan.id@gmail.com>
2023-04-19 11:00:49 +0700

    Upload

---
+++ app-files/data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4812947436884727,
+        "pitch": 0.23588263591734382,
+        "fov": 1.3815746638962287
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8658026930453104,
+          "pitch": 0.002947514214781677,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8679873133152514,
+        "pitch": 0.09448270414029025,
+        "fov": 1.3815746638962287
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.68601044215918,
+          "pitch": 0.048941046149636236,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
